75f291658d27386ec81261cfcc20caa5e3f95493
Naomi <naomipriest@rocketmail.com>
2020-02-14 17:03:25 -0500

    Finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -20,31 +20,81 @@
 
 const cardsCont = document.querySelector('.cards-container');
 console.log(cardsCont);
-cardsCont.appendChild(Cards(response.data));
 
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
-.then(response => {
-    console.log(response);
+  .then(response => {
+    console.log('here', response);
+    console.log(response.data.articles);
     
     Object(response.data.articles.javascript).map(item => {
-      axios.get(item.headline, item.authorPhoto, item.authorName)
-      .then(response => {
-        cardsCont.appendChild(Cards(response.data));
-       
-      })
-      console.log(response.data.articles.javascript);
-      console.log(response.data.articles.bootstrap);
-      console.log(response.data.articles.technology);
-      console.log(response.data.articles.jquery);
-      console.log(response.data.articles.node);
+        cardsCont.appendChild(Cards(item));
+     })
+     return response
+      
     })
-.catch(error => {
+  .catch(error => {
     console.log("code isn't running", error);
-});
+  });
+  
+  axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    console.log('here', response);
+    console.log(response.data.articles);
+    
+    Object(response.data.articles.bootstrap).map(item => {
+        cardsCont.appendChild(Cards(item));
+     })
+     return response
+      
+    })
+  .catch(error => {
+    console.log("code isn't running", error);
+  });
 
+  axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    console.log('here', response);
+    console.log(response.data.articles);
+    
+    Object(response.data.articles.technology).map(item => {
+        cardsCont.appendChild(Cards(item));
+     })
+     return response
+      
+    })
+  .catch(error => {
+    console.log("code isn't running", error);
+  });
 
+  axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    console.log('here', response);
+    console.log(response.data.articles);
+    
+    Object(response.data.articles.jquery).map(item => {
+        cardsCont.appendChild(Cards(item));
+     })
+     return response
+      
+    })
+  .catch(error => {
+    console.log("code isn't running", error);
+  });
 
-
+  axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    console.log('here', response);
+    console.log(response.data.articles);
+    
+    Object(response.data.articles.node).map(item => {
+        cardsCont.appendChild(Cards(item));
+     })
+     return response
+      
+    })
+  .catch(error => {
+    console.log("code isn't running", error);
+  });
 
 
 
@@ -52,27 +102,27 @@ function Cards(articles){
   const cardDiv = document.createElement('div');
   const headline = document.createElement('div');
   const author = document.createElement('div');
-  const imgCont = document.createElement('div');
+  const imgCont = document.createElement('div')
   const img = document.createElement('img');
   const by = document.createElement('span');
 
-  cardDiv.classList.add('card');
-  headline.classList.add('headline');
-  author.classList.add('author');
-  imgCont.classList.add('img-container');
+    cardDiv.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgCont.classList.add('img-container');
 
-  cardDiv.appendChild(headline);
-  cardDiv.appendChild(author);
-  author.appendChild(imgCont);
-  imgCont.appendChild(img);
-  author.appendChild(by);
+    cardDiv.appendChild(headline);
+    cardDiv.appendChild(author);
+    author.appendChild(imgCont);
+    imgCont.appendChild(img);
+    author.appendChild(by);
 
     
 
-  headline.textContent = `${articles.headline}`;
-  img.setAttribute('src', `${articles.authorPhoto}`);
-  by.textContent = `${articles.authorName}`;
+    headline.textContent = `${articles.headline}`;
+    img.setAttribute('src', `${articles.authorPhoto}`);
+    by.textContent = `${articles.authorName}`;
 
-  return cardDiv;
+    return cardDiv;
 
 }
